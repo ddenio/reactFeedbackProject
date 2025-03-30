28a2c6c2d408349cb9aa2b94fdf41f261933d397
Derin DeNio <derindenio@gmail.com>
2025-03-29 19:44:28 -0600

    Refactored event handlers

--- src/App.jsx
+++ src/App.jsx
@@ -9,29 +9,32 @@ function App() {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleGoodClick = () => {
-    const updatedGood = good + 1
-    setGood(updatedGood)
-  }
+  // const handleGoodClick = () => {
+  //   const updatedGood = good + 1
+  //   setGood(updatedGood)
+  // }
 
-  const handleNeutralClick = () => {
-    const updatedNeutral = neutral + 1
-    setNeutral(updatedNeutral)
-  }
+  // const handleNeutralClick = () => {
+  //   const updatedNeutral = neutral + 1
+  //   setNeutral(updatedNeutral)
+  // }
 
-  const handleBadClick = () => {
-    const updatedBad = bad + 1
-    setBad(updatedBad)
-  }
+  // const handleBadClick = () => {
+  //   const updatedBad = bad + 1
+  //   setBad(updatedBad)
+  // }
 
 
   return (
     <>
       <Display text="give feedback" />
       <div>
-        <Button onClick={handleGoodClick} text="good" />
+        <Button onClick={() => setGood(good+1)} text="good" />
+        <Button onClick={() => setNeutral(neutral+1)} text="neutral" />
+        <Button onClick={() => setBad(bad+1)} text="bad" />
+        {/* <Button onClick={handleGoodClick} text="good" />
         <Button onClick={handleNeutralClick} text="neutral" />
-        <Button onClick={handleBadClick} text="bad" />
+        <Button onClick={handleBadClick} text="bad" /> */}
       </div>
       <Display text="statistics" />
       <DisplaySmall text="good" num={good} />
